25151bea225bb728266fb05f26887dece60c4d2d
caohuichang <satrong@foxmail.com>
2021-09-27 09:18:38 +0800

    fix: add padding

--- src/template.ts
+++ src/template.ts
@@ -9,8 +9,9 @@ export default `
   <style>
   body{background:#eee;font-size:0.875rem;font-family:arial;}
   .d-flex-wrap{display:flex;flex-wrap:wrap;}
-  .nav{font-weight:bold;font-size:1rem;margin:1rem 0;padding:0.5rem 0;border-bottom:1px #ddd solid;position:sticky;top:0;background:#eee;}
+  .nav{font-weight:bold;font-size:1rem;margin:1rem 0;padding:0.5rem;border-bottom:1px #ddd solid;position:sticky;top:0;background:#eee;}
   .nav span:not(:last-child):after{content:'/';margin:0 5px;}
+  .wrap{padding:0 0.5rem;}
   .wrap > div{margin:5px 0;min-width:240px;width:25%;text-overflow:ellipsis;overflow:hidden;white-space:nowrap;}
   a {color:#1c68de;text-decoration:none;}
   a:hover{text-decoration:underline;color:#0d459a;}
